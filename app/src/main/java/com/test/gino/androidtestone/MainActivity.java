efec5f85b2029c1cf671932b089e5eec4d2912ba
Gino <neodracarys@gmail.com>
2016-05-06 00:56:40 +0800

    Test

---
+++ app/src/main/java/com/test/gino/androidtestone/MainActivity.java
@@ -0,0 +1,15 @@
+package com.test.gino.androidtestone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/// Hello World
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
